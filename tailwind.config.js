a08d89b9ab6a1e341e7d08a7c1d1a752d070cf30
Olamide <aboyejiolamide15@gmail.com>
2021-08-23 14:05:44 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors : {
+        'custom-orange' : '#DA562E'
+      }
+    },
+    container : {
+      center : true,
+      padding : '1.5rem'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
